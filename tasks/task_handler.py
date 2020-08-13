049ab1d9dd9767b1d2de59ddb4d9356c67687c33
HaolingZHANG <hlzchn@gmail.com>
2020-08-13 11:52:05 +0800

    update task_handler

--- tasks/task_handler.py
+++ tasks/task_handler.py
@@ -3,136 +3,136 @@ from ReverseEncodingTree.tasks.task_inform import *
 
 
 def run_imply():
-    task = Logic(method_type=METHOD_TYPE.N, logic_type=LOGIC_TYPE.IMPLY, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.N, logic_type=LogicType.IMPLY, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "IMPLY", METHOD_TYPE.N)
+    save_distribution(counts, "../output/", "IMPLY", MethodType.N)
 
-    task = Logic(method_type=METHOD_TYPE.FS, logic_type=LOGIC_TYPE.IMPLY, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.FS, logic_type=LogicType.IMPLY, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "IMPLY", METHOD_TYPE.FS)
+    save_distribution(counts, "../output/", "IMPLY", MethodType.FS)
 
-    task = Logic(method_type=METHOD_TYPE.BI, logic_type=LOGIC_TYPE.IMPLY, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.BI, logic_type=LogicType.IMPLY, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "IMPLY", METHOD_TYPE.BI)
+    save_distribution(counts, "../output/", "IMPLY", MethodType.BI)
 
-    task = Logic(method_type=METHOD_TYPE.GS, logic_type=LOGIC_TYPE.IMPLY, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.GS, logic_type=LogicType.IMPLY, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "IMPLY", METHOD_TYPE.GS)
+    save_distribution(counts, "../output/", "IMPLY", MethodType.GS)
 
 
 def run_nand():
-    task = Logic(method_type=METHOD_TYPE.N, logic_type=LOGIC_TYPE.NAND, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.N, logic_type=LogicType.NAND, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "NAND", METHOD_TYPE.N)
+    save_distribution(counts, "../output/", "NAND", MethodType.N)
 
-    task = Logic(method_type=METHOD_TYPE.FS, logic_type=LOGIC_TYPE.NAND, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.FS, logic_type=LogicType.NAND, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "NAND", METHOD_TYPE.FS)
+    save_distribution(counts, "../output/", "NAND", MethodType.FS)
 
-    task = Logic(method_type=METHOD_TYPE.BI, logic_type=LOGIC_TYPE.NAND, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.BI, logic_type=LogicType.NAND, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "NAND", METHOD_TYPE.BI)
+    save_distribution(counts, "../output/", "NAND", MethodType.BI)
 
-    task = Logic(method_type=METHOD_TYPE.GS, logic_type=LOGIC_TYPE.NAND, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.GS, logic_type=LogicType.NAND, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "NAND", METHOD_TYPE.GS)
+    save_distribution(counts, "../output/", "NAND", MethodType.GS)
 
 
 def run_nor():
-    task = Logic(method_type=METHOD_TYPE.N, logic_type=LOGIC_TYPE.NOR, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.N, logic_type=LogicType.NOR, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "NOR", METHOD_TYPE.N)
+    save_distribution(counts, "../output/", "NOR", MethodType.N)
 
-    task = Logic(method_type=METHOD_TYPE.FS, logic_type=LOGIC_TYPE.NOR, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.FS, logic_type=LogicType.NOR, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "NOR", METHOD_TYPE.FS)
+    save_distribution(counts, "../output/", "NOR", MethodType.FS)
 
-    task = Logic(method_type=METHOD_TYPE.BI, logic_type=LOGIC_TYPE.NOR, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.BI, logic_type=LogicType.NOR, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "NOR", METHOD_TYPE.BI)
+    save_distribution(counts, "../output/", "NOR", MethodType.BI)
 
-    task = Logic(method_type=METHOD_TYPE.GS, logic_type=LOGIC_TYPE.NOR, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.GS, logic_type=LogicType.NOR, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "NOR", METHOD_TYPE.GS)
+    save_distribution(counts, "../output/", "NOR", MethodType.GS)
 
 
 def run_xor():
-    task = Logic(method_type=METHOD_TYPE.N, logic_type=LOGIC_TYPE.XOR, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.N, logic_type=LogicType.XOR, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "XOR", METHOD_TYPE.N)
+    save_distribution(counts, "../output/", "XOR", MethodType.N)
 
-    task = Logic(method_type=METHOD_TYPE.FS, logic_type=LOGIC_TYPE.XOR, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.FS, logic_type=LogicType.XOR, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "XOR", METHOD_TYPE.FS)
+    save_distribution(counts, "../output/", "XOR", MethodType.FS)
 
-    task = Logic(method_type=METHOD_TYPE.BI, logic_type=LOGIC_TYPE.XOR, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.BI, logic_type=LogicType.XOR, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "XOR", METHOD_TYPE.BI)
+    save_distribution(counts, "../output/", "XOR", MethodType.BI)
 
-    task = Logic(method_type=METHOD_TYPE.GS, logic_type=LOGIC_TYPE.XOR, max_generation=500, display_results=False)
+    task = Logic(method_type=MethodType.GS, logic_type=LogicType.XOR, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "XOR", METHOD_TYPE.GS)
+    save_distribution(counts, "../output/", "XOR", MethodType.GS)
 
 
 def run_cart_pole_v0():
-    task = Game(method_type=METHOD_TYPE.N, game_type=GAME_TYPE.CartPole_v0, episode_steps=300, episode_generation=10,
+    task = Game(method_type=MethodType.N, game_type=GameType.CartPole_v0, episode_steps=300, episode_generation=10,
                 max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "CartPole_v0", METHOD_TYPE.N)
+    save_distribution(counts, "../output/", "CartPole_v0", MethodType.N)
 
-    task = Game(method_type=METHOD_TYPE.FS, game_type=GAME_TYPE.CartPole_v0, episode_steps=300, episode_generation=10,
+    task = Game(method_type=MethodType.FS, game_type=GameType.CartPole_v0, episode_steps=300, episode_generation=10,
                 max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "CartPole_v0", METHOD_TYPE.FS)
+    save_distribution(counts, "../output/", "CartPole_v0", MethodType.FS)
 
-    task = Game(method_type=METHOD_TYPE.BI, game_type=GAME_TYPE.CartPole_v0, episode_steps=300, episode_generation=10,
+    task = Game(method_type=MethodType.BI, game_type=GameType.CartPole_v0, episode_steps=300, episode_generation=10,
                 max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "CartPole_v0", METHOD_TYPE.BI)
+    save_distribution(counts, "../output/", "CartPole_v0", MethodType.BI)
 
-    task = Game(method_type=METHOD_TYPE.GS, game_type=GAME_TYPE.CartPole_v0, episode_steps=300, episode_generation=10,
+    task = Game(method_type=MethodType.GS, game_type=GameType.CartPole_v0, episode_steps=300, episode_generation=10,
                 max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "CartPole_v0", METHOD_TYPE.GS)
+    save_distribution(counts, "../output/", "CartPole_v0", MethodType.GS)
 
 
 def run_cart_pole_v0_with_attack():
     attacker = CartPole_v0_Attacker(attack_type=AttackType.Gaussian, gaussian_peak=1000)
     noise_level = 1
 
-    task = Game(method_type=METHOD_TYPE.FS, game_type=GAME_TYPE.CartPole_v0, episode_steps=500, episode_generation=20,
+    task = Game(method_type=MethodType.FS, game_type=GameType.CartPole_v0, episode_steps=500, episode_generation=20,
                 attacker=attacker, noise_level=noise_level, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "CartPole_v0", METHOD_TYPE.FS)
+    save_distribution(counts, "../output/", "CartPole_v0", MethodType.FS)
 
-    task = Game(method_type=METHOD_TYPE.BI, game_type=GAME_TYPE.CartPole_v0, episode_steps=300, episode_generation=10,
+    task = Game(method_type=MethodType.BI, game_type=GameType.CartPole_v0, episode_steps=300, episode_generation=10,
                 attacker=attacker, noise_level=noise_level, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "CartPole_v0", METHOD_TYPE.BI)
+    save_distribution(counts, "../output/", "CartPole_v0", MethodType.BI)
 
-    task = Game(method_type=METHOD_TYPE.GS, game_type=GAME_TYPE.CartPole_v0, episode_steps=300, episode_generation=10,
+    task = Game(method_type=MethodType.GS, game_type=GameType.CartPole_v0, episode_steps=300, episode_generation=10,
                 attacker=attacker, noise_level=noise_level, max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "CartPole_v0", METHOD_TYPE.GSS)
+    save_distribution(counts, "../output/", "CartPole_v0", MethodType.GSS)
 
 
 def run_lunar_lander_v0():
-    task = Game(method_type=METHOD_TYPE.N, game_type=GAME_TYPE.LunarLander_v2, episode_steps=100, episode_generation=2,
+    task = Game(method_type=MethodType.N, game_type=GameType.LunarLander_v2, episode_steps=100, episode_generation=2,
                 max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "LunarLander_v2", METHOD_TYPE.N)
+    save_distribution(counts, "../output/", "LunarLander_v2", MethodType.N)
 
-    task = Game(method_type=METHOD_TYPE.FS, game_type=GAME_TYPE.LunarLander_v2, episode_steps=100, episode_generation=2,
+    task = Game(method_type=MethodType.FS, game_type=GameType.LunarLander_v2, episode_steps=100, episode_generation=2,
                 max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "LunarLander_v2", METHOD_TYPE.FS)
+    save_distribution(counts, "../output/", "LunarLander_v2", MethodType.FS)
 
-    task = Game(method_type=METHOD_TYPE.BI, game_type=GAME_TYPE.LunarLander_v2, episode_steps=100, episode_generation=2,
+    task = Game(method_type=MethodType.BI, game_type=GameType.LunarLander_v2, episode_steps=100, episode_generation=2,
                 max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "LunarLander_v2", METHOD_TYPE.BI)
+    save_distribution(counts, "../output/", "LunarLander_v2", MethodType.BI)
 
-    task = Game(method_type=METHOD_TYPE.GS, game_type=GAME_TYPE.LunarLander_v2, episode_steps=100, episode_generation=2,
+    task = Game(method_type=MethodType.GS, game_type=GameType.LunarLander_v2, episode_steps=100, episode_generation=2,
                 max_generation=500, display_results=False)
     generations, counts = task.run(1000)
-    save_distribution(counts, "../output/", "LunarLander_v2", METHOD_TYPE.GS)
+    save_distribution(counts, "../output/", "LunarLander_v2", MethodType.GS)
